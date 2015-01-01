8374931fdb7a76eb02e6f44fe8640ab83c3f3dcb
Russell Hay <me@russellhay.com>
2014-12-31 17:49:06 -0800

    Adding Configuration Options

--- parachute.ino
+++ parachute.ino
@@ -1,18 +1,51 @@
-
 #include <Servo.h>
 #include <avr/sleep.h>
 #include <Bounce2.h>
 
+///////////////////////////////////////////////////////
+// Configuration Section
+//////////////////////////////////////////////////////
 
+////////// Options
+
+// Put device into sleep mode to reduce battery consumption after triggering.
+#define POWER_DOWN_AFTER_TRIGGER
+
+// Should the arm pin use the internal pullup
+#define USE_INTERNAL_PULLUP
+
+// Is arm LOW or HIGH on the switch
+#define TIGGER_VALUE LOW
+
+////////// Pin Configuration
+
+// Trigger Servo Pin
 #define SERVO_PIN 9
+
+// Ready LED (13 is the onboard led)
 #define READY_PIN 13
+
+// Switch, pulled up high
 #define ARM_PIN 2
+
+// 0G Logic pin off of the Accelerometer, used to detect free fall
 #define APOGEE_PIN 11
+
+// Analog pin for the Z-axis of the accelerometer
 #define VERTICAL_PIN A0
 
+////////// Tuning Points
+
+// Degree for the armed and ready position
 #define SERVO_READY 0
+
+// Degree for the deployed position to unhook the rubberband
 #define SERVO_DEPLOY 180
+
+// Don't trigger until we've hit at least this vertical acceleration
 #define ACCELERATION_THRESHOLD 600
+
+/////////////////////////////////////////////////////////
 
 Servo parachute;
 Bounce arm;
@@ -25,7 +58,9 @@ void setup()
 {
   Serial.begin(9600);
   pinMode(ARM_PIN, INPUT);
+#ifdef USE_INTERNAL_PULLUP
   digitalWrite(ARM_PIN, HIGH);  // Enable pull up resistor
+#endif // USE_INTERNAL_PULLUP
   
   // Accel data for Z axis, ignore all others.
   pinMode(VERTICAL_PIN, INPUT);
@@ -40,10 +75,30 @@ void setup()
   
 }
 
-void pinInterrupt()
-{
-  detachInterrupt(0);
-}
+#ifdef POWER_DOWN_AFTER_TRIGGER
+   void pinInterrupt()
+   {
+     detachInterrupt(0);
+   }
+   
+   inline void sleep()
+   {
+      attachInterrupt(0, pinInterrupt, HIGH);
+      delay(100);
+
+      // Enable us to power down after parachute deployment to save battery power.
+      set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+      sleep_enable();
+      sleep_mode();
+      sleep_disable();
+
+      // Reset the state
+      triggered = false;
+      parachute.write(SERVO_READY);
+   }
+#else // POWER_DOWN_AFTER_TRIGGER
+   inline void sleep() {}
+#endif // POWER_DOWN_AFTER_TRIGGER
 
 void loop_armed()
 {
@@ -66,17 +121,7 @@ void loop_armed()
       delay(1000);
 
       digitalWrite(READY_PIN, LOW);
-
-      attachInterrupt(0, pinInterrupt, HIGH);
-      delay(100);
-      set_sleep_mode(SLEEP_MODE_PWR_DOWN); // Enable us to power down after parachute deployment to save battery power.
-      sleep_enable();
-      sleep_mode();
-      sleep_disable();
-      
-      // Reset the state
-      triggered = false;
-      parachute.write(SERVO_READY);
+      sleep();
     }
   }
 
@@ -93,16 +138,16 @@ void loop_unarmed()
 void loop()
 {
   arm.update();
-  button_state = arm.read() == LOW;
+  button_state = arm.read() == TIGGER_VALUE;
   
   if (button_state != armed) {
     if (button_state) {
-      Serial.println("Arming");
-      // Transition to armed state
-      digitalWrite(READY_PIN, HIGH);
+         Serial.println("Arming");
+         // Transition to armed state
+         digitalWrite(READY_PIN, HIGH);
       } else {
-      // Transition to unarmed state
-      parachute.write(SERVO_READY);
+         // Transition to unarmed state
+         parachute.write(SERVO_READY);
     }   
     armed = button_state;
   }
